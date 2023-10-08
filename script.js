a459f7792a2d0bcdd0bf25e9e9ce3782df6db50b
SkyeUwU <79043495+SkyeUwU@users.noreply.github.com>
2023-10-09 01:24:22 +0300

    modified:   index.html 	deleted:    script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// var xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://api.skyethebot.xyz/botinfo', true);
-// xhr.send();
-
-// xhr.onload = function () {
-//     var res = JSON.parse(this.responseText);
-//     document.getElementById('avatar').src = res.dev_avatar;
-// }
